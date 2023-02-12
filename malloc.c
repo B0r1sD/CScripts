9472d5711ee422a87738bc5c1075a2cd920f7a29
Boris Depoortere <boris.depoortere@student.howest.be>
2023-02-12 23:37:52 +0100

    C Scripts made until lecture 6 of CS50

--- malloc.c
+++ malloc.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-
 #include <stdlib.h>
 
+//binky time
 int main(void)
 {
-    
+    //initialise pointers (that don't point to anything yet)
+    int *a;
+    int *b;
+
+    //allocate memmory space for the pointees = heap (use free to free up this space afterwards to prevent stack overflow)
+    a = malloc(sizeof(int));
+
+    *a = 42;
+    b = a;
+    *b = 13;
+
+    printf("a = %i, b = %i\n", *a, *b);
+    printf("a = %p, b = %p\n", a, b);
+
+    free(a);
 }
